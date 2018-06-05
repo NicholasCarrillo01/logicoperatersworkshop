35e87ba1304f39b3dab5f60267d2f4fdb8bdf744
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-06-05 15:26:44 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Relational operaters
+//        <  - less than
+//        <= - less than or equal to
+//        >  - greater than
+//        >= - greater than or equal to
+//        == - equals
+//        != - not equal
+
+//        logic Operaters
+//          && - and
+//          | | - or
+//           !  - Not
+
+        int a = 2;
+        int b = 4;
+        int x = 6;
+        int y = 1;
+
+        System.out.println("a = 2");
+        System.out.println("b = 4");
+        System.out.println("x = 6");
+        System.out.println("y = 1");
+
+        if (a == x && y + y == a);
+//        will this method run?
+          System.out.println("a == x && y +y == a");
+
+        if  (!(a == x) || y + y == a);
+         System.out.println("a == x || y + y == a");
+//         yes, this will run because one of our conditions is true.
+//         all you need is a single true condition for an or operater to be true
+
+                
+
+
+    }
+}
