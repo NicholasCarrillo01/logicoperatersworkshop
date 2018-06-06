72c588b595a8bb2d7f06bd2022b8f7c836e1215c
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-06-06 10:53:22 -0400

    worked on relational operaters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,16 +27,42 @@ public class Main {
         System.out.println("x = 6");
         System.out.println("y = 1");
 
-        if (a == x && y + y == a);
+        if (a == x && y + y == a) ;
 //        will this method run?
-          System.out.println("a == x && y +y == a");
+        System.out.println("a == x && y +y == a");
 
-        if  (!(a == x) || y + y == a);
-         System.out.println("a == x || y + y == a");
+        if (!(a == x) || y + y == a) ;
+        System.out.println("a == x || y + y == a");
 //         yes, this will run because one of our conditions is true.
 //         all you need is a single true condition for an or operater to be true
+//
 
-                
+
+//        less than
+        if (a < b) ;
+//           will this method run?
+        System.out.println("a <b");
+//          yes this will run because the value of a is less than the value of b
+
+//             less than or equal too
+        if (b <= 4) {
+            System.out.println("b <= 4");
+        }
+        if (b != 6) {
+          System.out.println("b != 6");
+        }
+
+
+        if (a >= 8) {
+         System.out.println("a >= 8");
+        }
+        if (x == 6) {
+        System.out.println("x == 6");
+        }
+//       greater than
+        if (a > 6) {
+        System.out.println("a > 6");
+        }
 
 
     }
